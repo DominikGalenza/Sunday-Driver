75a92dc4ea4b4ff31de905b0719cc140280c77f5
Dominik Gałenza <dominik.galenza@gmail.com>
2022-01-22 16:47:21 +0100

    Added responsive elements of the environment

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Car : MonoBehaviour
 {
@@ -20,5 +21,13 @@ public class Car : MonoBehaviour
     public void Steer(int value)
     {
         steerValue = value;
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
